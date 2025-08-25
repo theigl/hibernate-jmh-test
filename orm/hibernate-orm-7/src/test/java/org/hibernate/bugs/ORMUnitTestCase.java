434b4f511fc9ee9f0759b9a1361b9e993f4578ea
Thomas Heigl <thomas.heigl@gmail.com>
2025-08-25 11:54:58 +0200

    Add tests demonstrating issues with criteria equality and query plan caching

--- orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
+++ orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -1,13 +1,19 @@
 package org.hibernate.bugs;
 
 import org.hibernate.cfg.AvailableSettings;
-
-import org.hibernate.testing.orm.junit.DomainModel;
-import org.hibernate.testing.orm.junit.ServiceRegistry;
-import org.hibernate.testing.orm.junit.SessionFactory;
-import org.hibernate.testing.orm.junit.SessionFactoryScope;
-import org.hibernate.testing.orm.junit.Setting;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.query.SelectionQuery;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.stat.spi.StatisticsImplementor;
+import org.hibernate.testing.orm.junit.*;
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using its built-in unit test framework.
@@ -20,6 +26,7 @@ import org.junit.jupiter.api.Test;
  */
 @DomainModel(
 		annotatedClasses = {
+				ORMUnitTestCase.Book.class
 				// Add your entities here.
 				// Foo.class,
 				// Bar.class
@@ -36,20 +43,86 @@ import org.junit.jupiter.api.Test;
 				// For your own convenience to see generated queries:
 				@Setting(name = AvailableSettings.SHOW_SQL, value = "true"),
 				@Setting(name = AvailableSettings.FORMAT_SQL, value = "true"),
-				// @Setting( name = AvailableSettings.GENERATE_STATISTICS, value = "true" ),
-
-				// Add your own settings that are a part of your quarkus configuration:
-				// @Setting( name = AvailableSettings.SOME_CONFIGURATION_PROPERTY, value = "SOME_VALUE" ),
+				@Setting(name = AvailableSettings.QUERY_PLAN_CACHE_ENABLED, value = "true"),
+				@Setting(name = AvailableSettings.CRITERIA_PLAN_CACHE_ENABLED, value = "true"),
+				@Setting(name = AvailableSettings.CRITERIA_COPY_TREE, value = "false"),
+				@Setting(name = AvailableSettings.GENERATE_STATISTICS, value = "true"),
 		}
 )
 @SessionFactory
 class ORMUnitTestCase {
 
-	// Add your tests, using standard JUnit 5.
 	@Test
-	void hhh123Test(SessionFactoryScope scope) throws Exception {
-		scope.inTransaction( session -> {
-			// Do stuff...
-		} );
+	void criteriaValueQueryPlanMiss(SessionFactoryScope scope) {
+		// ValueBindJpaCriteriaParameter equality is based on value equality, rendering the query plan cache for criteria queries
+		// virtually ineffective.
+
+		scope.getSessionFactory().getStatistics().clear();
+		scope.inTransaction(session -> {
+			createBookSelectionQuery(session, "name1").getResultList();
+			createBookSelectionQuery(session, "name2").getResultList();
+			createBookSelectionQuery(session, "name3").getResultList();
+
+			final StatisticsImplementor stats = scope.getSessionFactory().getStatistics();
+			assertThat(stats.getQueryPlanCacheMissCount()).isEqualTo(1);
+			assertThat(stats.getQueryPlanCacheHitCount()).isEqualTo(2);
+		});
 	}
+
+	@Test
+	void criteriaEmbedQueryPlanMiss(SessionFactoryScope scope) {
+		// EmbeddedSqmPathSource acquires a unique alias, rendering the query plan cache ineffective 
+
+		scope.getSessionFactory().getStatistics().clear();
+		scope.inTransaction(session -> {
+			createBookEmbedSelectionQuery(session).getResultList();
+			createBookEmbedSelectionQuery(session).getResultList();
+			createBookEmbedSelectionQuery(session).getResultList();
+
+			final StatisticsImplementor stats = scope.getSessionFactory().getStatistics();
+			assertThat(stats.getQueryPlanCacheMissCount()).isEqualTo(1);
+			assertThat(stats.getQueryPlanCacheHitCount()).isEqualTo(2);
+		});
+	}
+
+	private static SelectionQuery<Book> createBookSelectionQuery(SessionImplementor session, String name) {
+		final HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+		final JpaCriteriaQuery<Book> q = cb.createQuery(Book.class);
+		final Root<Book> root = q.from(Book.class);
+		q.select(root);
+		q.where(cb.equal(root.get("name"), name));
+		return session.createSelectionQuery(q);
+	}
+
+	private static SelectionQuery<Book> createBookEmbedSelectionQuery(SessionImplementor session) {
+		final HibernateCriteriaBuilder cb = session.getCriteriaBuilder();
+		final JpaCriteriaQuery<Book> q = cb.createQuery(Book.class);
+		final Root<Book> root = q.from(Book.class);
+		q.select(root);
+		final Path<BookDetails> bookDetails = root.get("details"); // Note: the test passes with root.join("details")
+		q.where(cb.equal(bookDetails.get("info"), "anyName"));
+		return session.createSelectionQuery(q);
+	}
+
+	@Entity(name = "Book")
+	@Table(name = "Book")
+	public static class Book {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		public Long id;
+
+		@Column
+		public String name;
+
+		@Embedded
+		public BookDetails details;
+	}
+
+	@Embeddable
+	public static class BookDetails {
+		@Column
+		public String info;
+	}
+
+
 }
